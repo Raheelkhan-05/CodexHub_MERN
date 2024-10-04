b557c5bee8597cb9ca2b3c1466194c100b7d9fe2
Raheelkhan-05 <140732511+Raheelkhan-05@users.noreply.github.com>
2024-10-05 00:29:29 +0530

    Update api.js

--- Backend/api.js
+++ Backend/api.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://codexhub.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
